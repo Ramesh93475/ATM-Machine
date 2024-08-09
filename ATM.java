7043c16d6948d16fd5711c8a92295f2b94ce6f42
Ramesh93475 <119522898+Ramesh93475@users.noreply.github.com>
2024-08-09 20:13:06 +0530

    Add files via upload
    ATM program in Java to display ATM transactions. An ATM is an electronic telecommunications system that allows 
    customers of banking firms to conduct financial transactions. The user must choose a choice from the example, 
    withdraw money, deposit money, check your balance, and exit the available options.

---
+++ ATM.java
@@ -0,0 +1,26 @@
+public class ATM{
+    private double depositAmount;
+    private double balance;
+    private double withdrawAmount;
+    public ATM()
+    {
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public void setBalance(double balance){
+        this.balance=balance;
+    }
+    public double getDepositAmount(){
+        return depositAmount;
+    }
+    public void setDepositAmount(double depositAmount){
+        this.depositAmount=depositAmount;
+    }
+    public double getWithdrawAmount(){
+        return withdrawAmount;
+    }
+    public void setWithdraWamount(double withdrawAmount){
+        this.withdrawAmount=withdrawAmount;
+    }
+}
